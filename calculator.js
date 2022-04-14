6ff4ef01cfae6405cf2bc2f7fe0fc982b94f1247
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-04-14 17:35:55 +0800

    feat: add initial primary functions of the calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  numberOne = Number(num1);
+  numberTwo = Number(num2);
+  return numberOne + numberTwo;
+}
+
+function subtract(num1, num2) {
+  numberOne = Number(num1);
+  numberTwo = Number(num2);
+  return numberOne - numberTwo;
+}
+
+function multiply(num1, num2) {
+  numberOne = Number(num1);
+  numberTwo = Number(num2);
+  return numberOne * numberTwo;
+}
+
+function divide(num1, num2) {
+  numberOne = Number(num1);
+  numberTwo = Number(num2);
+  return numberOne / numberTwo;
+}
