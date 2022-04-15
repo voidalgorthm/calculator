84b38358bc0b37ec5d7aa3f276d2e61052e52a55
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-04-15 21:23:38 +0800

    feat: add events for all digits seen in the screen

--- calculator.js
+++ calculator.js
@@ -1,3 +1,27 @@
+const container = document.querySelector('#container');
+const display = container.querySelector('#display');
+const keypads = container.querySelector('#keypads');
+
+const screen = display.querySelector('#screen');
+const input = screen.querySelector('#input');
+const output = screen.querySelector('#output');
+
+const controls = keypads.querySelectorAll('.btn-controls');
+const digits = keypads.querySelectorAll('.btn-digits');
+const operators = keypads.querySelectorAll('.btn-operators');
+const alters = keypads.querySelectorAll('.btn-alters');
+
+let numberOne;
+let numberTwo;
+let operator;
+
+digits.forEach(key => {
+  key.addEventListener('click', (e) => {
+    input.textContent += e.target.value;
+  })
+});
+
+
 function addition(num1, num2) {
   numberOne = Number(num1);
   numberTwo = Number(num2);
@@ -29,7 +53,7 @@ function operate(operator, num1, num2) {
       let sum = addition(num1, num2);
       console.log(sum);
       break;
-  
+
     default:
       break;
   }
